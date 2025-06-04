6a37a0da28564154b3a3137566503093fe5d13f9
Gabriel Dib <31329383+gabrieldib@users.noreply.github.com>
2025-06-04 17:29:32 +0000

    updated flattening()

--- src/flattening.js
+++ src/flattening.js
@@ -4,7 +4,7 @@ const text_node = figma.createText()
 text_node.x = 0; text_node.y = 0
 text_node.characters = 'Quick brown fox'
 
-temp_array = [] // because an anonymous array won't work. eg: figma.flatten([text_node], parent)
+temp_array = [] // because an anonymous array may not work in certain cases. eg: figma.flatten([text_node], parent)
 temp_array.push(text_node)
 
 figma.flatten(temp_array, figma.currentPage)    
