b85099ab19a9491656cb84a5642524d478624a0e
Gabriel Dib <31329383+gabrieldib@users.noreply.github.com>
2025-06-04 17:28:41 +0000

    Updated the create_clean_frame()

--- src/create_clean_frame.js
+++ src/create_clean_frame.js
@@ -3,10 +3,17 @@ function clone(val) {
   return JSON.parse(JSON.stringify(val));
 }
 
+// my old method, no shame on the beginner ways
 function create_clean_frame() {
   let the_frame = figma.createFrame();
   let Fills = clone(the_frame.fills);
   Fills.pop();
   the_frame.fills = Fills;
   return the_frame;
+}
+
+//alternatively
+function create_cleanest_frame() {
+  let the_frame = figma.createFrame()
+  the_frame.fills = []
 }
